698b9f37d784b398330e9138061d02516e47c3a7
Alejandro García <alejandro.garcia@finerio.mx>
2020-04-27 16:10:29 -0500

    Added 'UserApiData' entity

---
+++ micronaut/finerio-connect-lite/src/main/groovy/com/finerioconnect/lite/domain/UserApiData.groovy
@@ -0,0 +1,25 @@
+package com.finerioconnect.lite.domain
+
+import grails.gorm.annotation.Entity
+import io.micronaut.security.authentication.providers.UserState
+import org.grails.datastore.gorm.GormEntity
+
+@Entity 
+class UserApiData implements GormEntity<User> { 
+
+  User user
+  String username
+  String password
+  String clientId
+  String clientSecret
+  Date dateCreated
+  Date lastUpdated
+
+  static constraints = { 
+    username nullable: false, blank: false
+    password nullable: false, blank: false
+    clientId nullable: false, blank: false
+    clientSecret nullable: false, blank: false
+  }
+
+}

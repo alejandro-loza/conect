ebd7f523125bf10307dfc6d65a3da0c77484cc14
Alejandro García <alejandro.garcia@finerio.mx>
2020-05-08 23:29:15 -0500

    Added exception handler for wrong path variables

---
+++ micronaut/finerio-connect-lite/src/main/groovy/com/finerioconnect/lite/controllers/ConversionErrorExceptionHandler.groovy
@@ -0,0 +1,40 @@
+package com.finerioconnect.lite.controllers
+
+import com.finerioconnect.lite.dtos.Error
+import com.finerioconnect.lite.dtos.Errors
+import com.finerioconnect.lite.services.MessageService
+
+import io.micronaut.context.annotation.Replaces
+import io.micronaut.core.convert.exceptions.ConversionErrorException
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.server.exceptions.ExceptionHandler
+
+import io.micronaut.http.server.exceptions.ConversionErrorHandler
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+@Replaces(ConversionErrorHandler)
+class ConversionErrorExceptionHandler
+    implements ExceptionHandler<ConversionErrorException, HttpResponse>{
+
+  @Inject
+  MessageService messageService
+
+  @Override
+  HttpResponse handle( HttpRequest request, ConversionErrorException e ) {
+
+    def message = 'url.invalid'
+    def error = new Error()
+    error.code = message
+    error.title = messageService.getMessage( message )
+    error.detail = messageService.getMessage( "${message}.detail" )
+    def errors = new Errors()
+    errors.errors = [ error ]
+    return HttpResponse.badRequest( errors )
+
+  }
+
+}
